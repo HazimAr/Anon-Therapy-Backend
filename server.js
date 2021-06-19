6b8a82e20c10663ac62efa6f2e58c4c972b6b06f
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-18 23:07:11 -0700

    Messaging works

--- server.js
+++ server.js
@@ -1,10 +1,80 @@
 const io = require("socket.io")(process.env.PORT || 6969, {
   cors: {
-    origin: ["http://localhost:3000", "https://anon-therapy.vercel.app"],
+    origin: ["http://localhost:3000", "https://anon-therapy.vercel.app/"],
   },
 });
 
+const rooms = [];
 io.on("connection", (socket) => {
   // socket.disconnect();
   // return;
+
+  let started = false;
+  let currentRoom = "";
+
+  socket.on("message", (message, room) => {
+    if (room) {
+      let found = false;
+      rooms.forEach((v) => {
+        if (v.name === room) {
+          v.users.forEach((usr) => {
+            if (usr.id === socket.id) {
+              found = true;
+            }
+          });
+        }
+      });
+
+      if (found) socket.to(room).except(socket.id).emit("message", message);
+    }
+  });
+
+  socket.on("join-room", (room, callback) => {
+    console.log(socket.id);
+    let found = null;
+    rooms.forEach((r) => {
+      if (r.name === room) {
+        found = r;
+      }
+    });
+    if (found) {
+      found.users.forEach((user) => {
+        if (user.id == socket.id) return;
+      });
+      if (found.users.length < 2) {
+        found.users.push({ id: socket.id, role: true });
+        socket.join(room);
+
+        currentRoom = room;
+        callback(true, true);
+        socket.to(room).emit("start");
+      } else {
+        socket.send("full");
+      }
+    } else {
+      rooms.push({
+        name: room,
+        users: [
+          {
+            id: socket.id,
+            role: false,
+          },
+        ],
+      });
+      socket.join(room);
+      currentRoom = room;
+
+      callback(false, false);
+    }
+    console.log(rooms[0].users);
+  });
+
+  socket.on("disconnect", () => {
+    rooms.forEach((room, i) => {
+      if (room.name === currentRoom) {
+        rooms.splice(i, 1);
+      }
+    });
+    socket.to(currentRoom).emit("leave");
+  });
 });
